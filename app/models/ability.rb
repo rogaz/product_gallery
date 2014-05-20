1a75f5b9b29c32cde334bdac35c0e4cd4e4be7d5
Raúl Ogaz <raul260290@gmail.com>
2014-05-19 21:47:37 -0600

    Se modifica layout y estructura para mostrar imágenes de producto.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
       user ||= User.new # guest user (not logged in)
       if user.new_record?
         can :index, Product
+        can :show, Product
       else
         can :manage, :all
       end
