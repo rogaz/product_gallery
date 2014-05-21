f4934548395d308359fb119f61dc314fd13003ce
Raúl Ogaz <raul260290@gmail.com>
2014-05-20 22:12:20 -0600

    Se implementa validación en tamaño de imágen.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class Picture < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :product_id, :attachment
@@ -6,4 +7,19 @@ class Picture < ActiveRecord::Base
 
   has_attached_file :attachment, :styles => { large: '600x600>', medium: '300x300>', thumb: '100x100>' }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
+
+  validate :check_dimensions
+
+  def check_dimensions
+    dim = {x: 800, y: 600}
+    temp_file = attachment.queued_for_write[:original]
+    unless temp_file.nil?
+      dimensions = Paperclip::Geometry.from_file(temp_file)
+      width = dimensions.width
+      height = dimensions.height
+      if width != dim[:x] && height != dim[:y]
+        errors.add(:attachment, "debe ser de #{dim[:x]}x#{dim[:y]} la imágen que seleccionó mide #{width.to_i.to_s}x#{height.to_i.to_s}")
+      end
+    end
+  end
 end
